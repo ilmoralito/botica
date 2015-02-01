2667f3d2508914f615b38ff5a0ff0e0d3c754c27
Mario Martinez <amakenadog@gmail.com>
2015-02-01 14:52:15 -0600

    Added new test users

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,17 +5,32 @@ class BootStrap {
   	def adminRole =  new Role(authority:"ROLE_ADMIN").save(failOnError:true)
   	def userRole =  new Role(authority:"ROLE_USER").save(failOnError:true)
 
-  	def testUser = new User(
+  	def john = new User(
       username:"me",
       fullName:"John Doe",
       password:"password"
     ).save(failOnError:true)
 
-  	UserRole.create testUser, adminRole, true
+    def juan = new User(
+      username:"juan",
+      fullName:"Juan Perez",
+      password:"password",
+      enabled:false
+    ).save(failOnError:true)
 
-  	assert User.count() == 1
+    def fulano = new User(
+      username:"fulano",
+      fullName:"Fulano de Tal",
+      password:"password"
+    ).save(failOnError:true)
+
+    UserRole.create john, adminRole, true
+    UserRole.create juan, adminRole, true
+  	UserRole.create fulano, userRole, true
+
+  	assert User.count() == 3
   	assert Role.count() == 2
-  	assert UserRole.count() == 1
+  	assert UserRole.count() == 3
   }
   def destroy = {
   }
